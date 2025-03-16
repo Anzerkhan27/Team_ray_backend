c89e1c1e77442d006d7c21ce06e150750cbb411d
Mohammad Anzer <U1969730@unimail.hud.ac.uk>
2025-03-16 18:36:52 +0000

    Add CSRF_TRUSTED_ORIGINS for production domain

--- team_ray/settings.py
+++ team_ray/settings.py
@@ -156,3 +156,8 @@ CORS_ALLOWED_ORIGINS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://web-production-7860.up.railway.app",
+]
